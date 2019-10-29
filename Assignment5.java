33a864baf950a9609dfc6c892d3004a92f911178
Matheus Vazzoler <44783953+VazzolerMatheus@users.noreply.github.com>
2019-10-28 17:58:40 -0700

    Update Assignment5.java

--- Assignment5.java
+++ Assignment5.java
@@ -1,9 +1,28 @@
-
 public class Assignment5 {
 
 	public static void main(String[] args) {
-		Testing
+		System.out.println(season(2, 10));
+		
+		 Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		    System.out.println("Enter username");
+
+		    String userName = myObj.nextLine();  // Read user input
+		    System.out.println("Username is: " + userName);  // Output user input
 
 	}
-
+	
+	public static void printGPA()
+	
+	public static String season(int month, int day) {
+		if(month >=3 && month <= 5) {
+			return "Spring";
+		}else if (month >= 6 && month <= 8) {
+			return "Summer";
+		}else if (month >= 9 && month <= 11) {
+			return "Fall";
+		}else {
+			return "Winter";
+		}
+		
+	}
 }
